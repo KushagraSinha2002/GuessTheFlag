2b26fd8f00de7ce5c467ec57581875906ae0d7fa
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-10-21 20:44:08 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kushagra Sinha on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
