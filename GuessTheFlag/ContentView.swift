39efc795d61cff1bade1ecdd2055c14d09ab4ff5
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-10-29 23:25:32 +0530

    color palette update

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -21,7 +21,8 @@ struct ContentView: View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                .init(color: Color(.white), location: 1)
             ],  center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
