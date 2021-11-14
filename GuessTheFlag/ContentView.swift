dc77c69b7b5f1883142bb99d64b22bc6ac5fca8b
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-11-14 21:03:53 +0530

    Color changed to black

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-                .init(color: Color(.white), location: 1)
+                .init(color: Color(.black), location: 1)
             ],  center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
